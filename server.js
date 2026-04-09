59b73ae5ca3048d8c25dc60faa3b2e9e2cc5b07d
Anuja Kotnala <anujakotnala2209@gmail.com>
2026-04-09 09:36:09 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express=require("express");
+const db = require("./db");
+
+const app=express();
+
+app.use(express.json());
+
+app.post("/addSchool", (req,res)=>{
+
+    const { name, address, latitude, longitude } = req.body;
+    if (!name || !address || latitude == null || longitude == null) {
+        return res.status(400).json({ message: "All fields required" });
+    }
+
+    if (typeof name !== "string" || typeof address !== "string") {
+    return res.status(400).json({ message: "Name and address must be strings" });
+}
+
+
+    if (typeof latitude !== "number" || typeof longitude !== "number") {
+        return res.status(400).json({ message: "Latitude and Longitude must be numbers" });
+    }
+
+    const sql = "INSERT INTO schools (name,address,latitude,longitude) VALUES (?,?,?,?)";
+
+    db.query(sql, [name, address, latitude, longitude], (err, result) => {
+
+        if (err) {
+            return res.status(500).json(err);
+        }
+    
+    res.json({message: "School has been added successfully"});
+    });
+});
+
+
+app.get("/listSchools", (req,res)=>{
+    const{latitude, longitude}=req.query;
+
+    if (!latitude || !longitude) {
+        return res.status(400).json({ message: "Location required" });
+    }
+
+     db.query("SELECT * FROM schools", (err, schools) => {
+
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        const userLat = parseFloat(latitude);
+        const userLon = parseFloat(longitude);
+
+        schools.forEach(school => {
+
+            const dLat = school.latitude - userLat;
+            const dLon = school.longitude - userLon;
+
+            school.distance = Math.sqrt(dLat * dLat + dLon * dLon);
+        });
+
+        schools.sort((a, b) => a.distance - b.distance);
+
+        res.json(schools);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
